b731608e09188ac627735f47f44584a18d815aee
xk <129093805+xkaigo@users.noreply.github.com>
2024-03-12 10:11:13 +0800

    Update Header.js
    kkk

--- themes/simple/components/Header.js
+++ themes/simple/components/Header.js
@@ -23,7 +23,7 @@ export default function Header (props) {
 
                         <div className='flex-col flex justify-center'>
                             <div className='text-2xl font-serif dark:text-white py-2 hover:scale-105 transform duration-200'>{siteConfig('AUTHOR')}</div>
-                            <div className='font-light dark:text-white py-2 hover:scale-105 transform duration-200 text-center' dangerouslySetInnerHTML={{ __html: siteConfig('SIMPLE_LOGO_DESCRIPTION', null, CONFIG) }} />
+                            //<div className='font-light dark:text-white py-2 hover:scale-105 transform duration-200 text-center' dangerouslySetInnerHTML={{ __html: siteConfig('SIMPLE_LOGO_DESCRIPTION', null, CONFIG) }} />
                         </div>
                     </div>
                 </Link>
