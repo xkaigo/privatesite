0c607a31d14c13c70d2651d4046ae9f231a54efe
xk <129093805+xkaigo@users.noreply.github.com>
2024-03-13 15:51:59 +0800

    Update ArticleDetail.js

--- themes/fukasawa/components/ArticleDetail.js
+++ themes/fukasawa/components/ArticleDetail.js
@@ -101,10 +101,10 @@ export default function ArticleDetail(props) {
 
       {post?.type === 'Post' && <ArticleAround prev={prev} next={next} /> }
 
-      {/* 评论互动 */}
-      <div className="duration-200 shadow py-6 px-12 w-screen md:w-full overflow-x-auto dark:border-gray-700 bg-white dark:bg-hexo-black-gray">
-        <Comment frontMatter={post} />
-      </div>
+    //  {/* 评论互动 */}
+    //  <div className="duration-200 shadow py-6 px-12 w-screen md:w-full overflow-x-auto dark:border-gray-700 bg-white dark:bg-hexo-black-gray">
+    //    <Comment frontMatter={post} />
+    //  </div>
     </div>
   )
 }
