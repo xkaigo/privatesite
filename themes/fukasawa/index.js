030e6102a7ff2ebf347ef4a7d24c5ebcc05c3afa
xk <129093805+xkaigo@users.noreply.github.com>
2024-03-13 12:46:52 +0800

    Update index.js

--- themes/fukasawa/index.js
+++ themes/fukasawa/index.js
@@ -207,4 +207,58 @@ const LayoutCategoryIndex = (props) => {
         </div>
         <div id='category-list' className='duration-200 flex flex-wrap'>
           {categoryOptions?.map(category => {
-            return
+            return (
+              <Link
+                key={category.name}
+                href={`/category/${category.name}`}
+                passHref
+                legacyBehavior>
+                <div
+                  className={'hover:text-black dark:hover:text-white dark:text-gray-300 dark:hover:bg-gray-600 px-5 cursor-pointer py-2 hover:bg-gray-100'}>
+                  <i className='mr-4 fas fa-folder' />{category.name}({category.count})
+                </div>
+              </Link>
+            )
+          })}
+        </div>
+      </div>
+    </>
+  )
+}
+
+/**
+ * 标签列表
+ * @param {*} props
+            * @returns
+            */
+const LayoutTagIndex = (props) => {
+  const { locale } = useGlobal()
+  const { tagOptions } = props
+  return <>
+    <div className='bg-white dark:bg-gray-700 px-10 py-10 shadow'>
+      <div className='dark:text-gray-200 mb-5'><i className='mr-4 fas fa-tag' />{locale.COMMON.TAGS}:</div>
+      <div id="tags-list" className="duration-200 flex flex-wrap ml-8">
+        {tagOptions.map(tag => {
+          return (
+            <div key={tag.name} className="p-2">
+              <TagItemMini key={tag.name} tag={tag} />
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  </>
+}
+
+export {
+  CONFIG as THEME_CONFIG,
+  LayoutBase,
+  LayoutIndex,
+  LayoutSearch,
+  LayoutArchive,
+  LayoutSlug,
+  Layout404,
+  LayoutPostList,
+  LayoutCategoryIndex,
+  LayoutTagIndex
+}
